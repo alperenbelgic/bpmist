3c8c0d62d68ee8d6b8839c53923d0f89ae448149
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2020-04-08 22:47:30 +0100

    Angular material installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ProcessDesignerComponent } from './process-designer/process-designer.component';
 import { ProcessItemComponent } from './process-item/process-item.component';
 import { ProcessItemSettingsComponent } from './process-item-settings/process-item-settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProcessItemSettingsComponent } from './process-item-settings/process-it
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
