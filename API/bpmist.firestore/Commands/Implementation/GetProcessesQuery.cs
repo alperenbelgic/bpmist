a0228c578d5dbb1ac8024dabe769cfc9ebe08af7
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2020-07-05 18:15:39 +0100

    Some changes.

--- API/bpmist.firestore/Commands/Implementation/GetProcessesQuery.cs
+++ API/bpmist.firestore/Commands/Implementation/GetProcessesQuery.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using bpmist.common.Commands;
 using bpmist.data.ICommands;
+using bpmist.firestore.DataModels;
+using Google.Cloud.Firestore;
 
 namespace bpmist.firestore.Commands
 {
@@ -11,7 +13,23 @@ namespace bpmist.firestore.Commands
     {
         protected override async Task<GetProcessesResult> ExecuteImplementationAsync(GetProcessesParameter parameter, IContextInformation contextInformation)
         {
-            throw new NotImplementedException();
+            var processesSnapshot =
+                await
+                FirestoreDb
+                .Create("bpmistproject")
+                .Collection("organisations")
+                .Document("I8b23jRR3LVAa6ROcqS8")
+                .Collection("processes")
+                .GetSnapshotAsync();
+
+            var processes = new FirestoreHelper().Get<Process>(processesSnapshot);
+
+            return
+            new GetProcessesResult(
+                processes.Select(p => new GetProcesses_ProcessesResult(
+                    p.ProcessName,
+                    p.Tasks.Select(t => new GetProcesses_Processes_TasksResult(t.TaskId, t.TaskName)).ToArray()
+                )).ToArray());
         }
 
         protected override async Task<IEnumerable<OperationErrorInformation>> ValidateAsync(GetProcessesParameter parameter, IContextInformation contextInformation)
@@ -19,4 +37,8 @@ namespace bpmist.firestore.Commands
             return Enumerable.Empty<OperationErrorInformation>();
         }
     }
+
+
+
+
 }
---
+++ API/bpmist.firestore/DataModels/FirebaseDb.cs
@@ -0,0 +1,66 @@
+﻿using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bpmist.firestore.DataModels
+{
+    public class FirestoreHelper
+    {
+        public T Get<T>(DocumentSnapshot documentSnapshot) where T : FirestoreDoc
+        {
+            var obj = documentSnapshot.ConvertTo<T>();
+
+            obj.Id = documentSnapshot.Id;
+
+            return obj;
+        }
+
+        public IEnumerable<T> Get<T>(QuerySnapshot querySnapshot) where T : FirestoreDoc
+        {
+            return querySnapshot.Select(documentSnapshot => Get<T>(documentSnapshot));
+        }
+    }
+
+    public class FirestoreDoc
+    {
+        public string Id { get; set; }
+
+        //created, updated, etc
+    }
+
+    [FirestoreData]
+    public class Process : FirestoreDoc
+    {
+        Process()
+        {
+
+        }
+
+        Process(DocumentSnapshot documentSnapshot)
+        {
+            this.MemberwiseClone();
+        }
+
+
+
+        [FirestoreProperty]
+        public string ProcessName { get; set; }
+
+        [FirestoreProperty]
+        public Task[] Tasks { get; set; }
+    }
+
+    [FirestoreData]
+    public class Task
+    {
+
+        [FirestoreProperty]
+        public string TaskName { get; set; }
+
+        [FirestoreProperty]
+        public string TaskId { get; set; }
+    }
+}
