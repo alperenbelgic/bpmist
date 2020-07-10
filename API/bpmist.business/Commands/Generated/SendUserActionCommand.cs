4b0248ec80463a63909396a5826e44560104455a
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2020-07-10 01:07:38 +0100

    GetProcessInstance Query injected in Send User Action command

--- API/bpmist.business/Commands/Generated/SendUserActionCommand.cs
+++ API/bpmist.business/Commands/Generated/SendUserActionCommand.cs
@@ -10,12 +10,14 @@ namespace bpmist.business.Commands
     Command<SendUserActionParameter, SendUserActionResult>, ISendUserActionCommand
     {
         public SendUserActionCommand(
-            bpmist.data.ICommands.IGetProcessInstanceQuery _GetProcessInstanceQuery
+            bpmist.data.ICommands.IGetProcessInstanceQuery _GetProcessInstanceQuery, bpmist.data.ICommands.IGetOrganizationUserQuery _GetOrganizationUserQuery
         )
         {
             this.GetProcessInstanceQuery = _GetProcessInstanceQuery;
+            this.GetOrganizationUserQuery = _GetOrganizationUserQuery;
         }
 
         private bpmist.data.ICommands.IGetProcessInstanceQuery GetProcessInstanceQuery { get; }
+        private bpmist.data.ICommands.IGetOrganizationUserQuery GetOrganizationUserQuery { get; }
     }
 }
