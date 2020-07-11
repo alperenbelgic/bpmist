3c99a33ce54e1d113a0bed609bf01f0515d83296
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2020-07-11 15:13:36 +0100

    - Actions included in StartNewProcess result

--- API/bpmist.common/CommandInterfaces/IStartNewProcessCommand.cs
+++ API/bpmist.common/CommandInterfaces/IStartNewProcessCommand.cs
@@ -21,18 +21,34 @@ namespace bpmist.common.ICommands
 
 public class StartNewProcessResult
 {
-    public StartNewProcessResult(string ProcessName, string ProcessInstanceId, string TaskName, string TaskInstanceId)
+    public StartNewProcessResult(string ProcessName, string ProcessInstanceId, string TaskName, string TaskInstanceId, StartNewProcess_ActionsResult[] Actions)
     {
             this.ProcessName = ProcessName;
             this.ProcessInstanceId = ProcessInstanceId;
             this.TaskName = TaskName;
             this.TaskInstanceId = TaskInstanceId;
+            this.Actions = Actions;
     }
 
         public string ProcessName { get; } 
         public string ProcessInstanceId { get; } 
         public string TaskName { get; } 
         public string TaskInstanceId { get; } 
+        public StartNewProcess_ActionsResult[] Actions { get; } 
+}
+
+public class StartNewProcess_ActionsResult
+{
+    public StartNewProcess_ActionsResult(string ActionText, string ActionType, string ActionId)
+    {
+            this.ActionText = ActionText;
+            this.ActionType = ActionType;
+            this.ActionId = ActionId;
+    }
+
+        public string ActionText { get; } 
+        public string ActionType { get; } 
+        public string ActionId { get; } 
 }
 
 
