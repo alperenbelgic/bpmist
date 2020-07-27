ece8756ae104337664bb48f326841c802b6566a8
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2020-07-27 12:10:23 +0100

    spa fallback

--- API/bpmist.webapi/Startup.cs
+++ API/bpmist.webapi/Startup.cs
@@ -63,6 +63,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
             });
 
         }
