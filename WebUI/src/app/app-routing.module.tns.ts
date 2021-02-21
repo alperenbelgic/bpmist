29e4467d6e6f8c70d32c9656022bb2520b7eab9e
alperenbelgic@outlook.com <alperenbelgic@outlook.com>
2021-02-21 19:53:33 +0000

    new routes included.

--- WebUI/src/app/app-routing.module.tns.ts
+++ WebUI/src/app/app-routing.module.tns.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "@nativescript/angular";
 import { ListProcessesComponent } from "./components/list-processes/list-processes.component";
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
 
 const routes: Routes = [
     { path: '', component: ListProcessesComponent },
+    { path: 'NewProcess/:processId', component: EditTaskComponent },
+    { path: 'EditTask/:processId/:processInstanceId/:taskInstanceId', component: EditTaskComponent },
     { path: "asdf", redirectTo: "/home", pathMatch: "full" },
     { path: "home", loadChildren: () => import("./home/home.module").then((m) => m.HomeModule) }
 ];
